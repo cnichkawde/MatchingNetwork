d16f963c1bdb20481c68a36aab0787841df4541c
Chetan Nichkawde (Digital) <CH349620@wipro.com>
2017-07-17 17:00:45 +0530

    moved classes to another file

--- matchingnetwork.py
+++ matchingnetwork.py
@@ -8,15 +8,13 @@ from keras.models import Model
 from keras.layers import Flatten, Input, Lambda
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-import tensorflow as tf
-from keras.layers.merge import _Merge
+from matchnn import MatchCosine
 
 bsize = 32 # batch size
 classes_per_set = 5 # classes per set or 5-way
 samples_per_class = 1 # samples per class 1-short
 
-data = dataset.OmniglotNShotDataset(batch_size=bsize,classes_per_set=classes_per_set,samples_per_class=samples_per_class,
-                                    trainsize=64000,valsize=20000)
+data = dataset.OmniglotNShotDataset(batch_size=bsize,classes_per_set=classes_per_set,samples_per_class=samples_per_class,trainsize=64000,valsize=20000)
 
 # Image embedding using Deep Convolutional Network
 conv1 = Conv2D(64,(3,3),padding='same',activation='relu')
@@ -50,94 +48,6 @@ def convembedding(x):
     x = fltn(x)
     
     return x
-
-class MatchCosine(_Merge):
-    """
-        Matching network with cosine similarity metric
-    """
-    def __init__(self,nway=5,**kwargs):
-        super(MatchCosine,self).__init__(**kwargs)
-        self.eps = 1e-10
-        self.nway = nway
-
-    def build(self, input_shape):
-        if not isinstance(input_shape, list) or len(input_shape) != self.nway+2:
-            raise ValueError('A ModelEuclidean layer should be called on a list of inputs of length %d'%(self.nway+2))
-
-    def call(self,inputs):
-        """
-        inputs in as array which contains the support set the embeddings, the target embedding as the second last value in the array, and true class of target embedding as the last value in the array
-        """ 
-        similarities = []
-
-        targetembedding = inputs[-2] # embedding of the query image
-        numsupportset = len(inputs)-2
-        for ii in range(numsupportset):
-            supportembedding = inputs[ii] # embedding for i^{th} member in the support set
-
-            sum_support = tf.reduce_sum(tf.square(supportembedding), 1, keep_dims=True)
-            supportmagnitude = tf.rsqrt(tf.clip_by_value(sum_support, self.eps, float("inf"))) #reciprocal of the magnitude of the member of the support 
-
-            sum_query = tf.reduce_sum(tf.square(targetembedding), 1, keep_dims=True)
-            querymagnitude = tf.rsqrt(tf.clip_by_value(sum_query, self.eps, float("inf"))) #reciprocal of the magnitude of the query image
-
-            dot_product = tf.matmul(tf.expand_dims(targetembedding,1),tf.expand_dims(supportembedding,2))
-            dot_product = tf.squeeze(dot_product,[1])
-
-            cosine_similarity = dot_product*supportmagnitude*querymagnitude
-            similarities.append(cosine_similarity)
-
-        similarities = tf.concat(axis=1,values=similarities)
-        softmax_similarities = tf.nn.softmax(similarities)
-        preds = tf.squeeze(tf.matmul(tf.expand_dims(softmax_similarities,1),inputs[-1]))
-        
-        preds.set_shape((inputs[0].shape[0],self.nway))
-
-        return preds
-
-    def compute_output_shape(self,input_shape):
-        input_shapes = input_shape
-        return (input_shapes[0][0],self.nway)
-
-# Bonus: Matching network with Euclidean metrtic
-class ModelEuclidean(_Merge):
-    """
-        Matching network with Euclidean metric
-    """
-    def __init__(self,nway=5,**kwargs):
-        super(ModelEuclidean,self).__init__(**kwargs)
-        self.eps = 1e-10
-        self.nway = nway
-
-    def build(self, input_shape):
-        if not isinstance(input_shape, list) or len(input_shape) != self.nway+2:
-            raise ValueError('A ModelEuclidean layer should be called on a list of inputs of length %d'%(self.nway+2))
-
-    def call(self,inputs):
-        """
-        inputs in as array which contains the support set the embeddings, the target embedding as the second last value in the array, and true class of target embedding as the last value in the array
-        """ 
-        similarities = []
-
-        targetembedding = inputs[-2]
-        numsupportset = len(inputs)-2
-        for ii in range(numsupportset):
-            supportembedding = inputs[ii]
-            dd = tf.negative(tf.sqrt(tf.reduce_sum(tf.square(supportembedding-targetembedding),1,keep_dims=True)))
-
-            similarities.append(dd)
-
-        similarities = tf.concat(axis=1,values=similarities)
-        softmax_similarities = tf.nn.softmax(similarities)
-        preds = tf.squeeze(tf.matmul(tf.expand_dims(softmax_similarities,1),inputs[-1]))
-        
-        preds.set_shape((inputs[0].shape[0],self.nway))
-
-        return preds
-
-    def compute_output_shape(self,input_shape):
-        input_shapes = input_shape
-        return (input_shapes[0][0],self.nway)
 
 numsupportset = samples_per_class*classes_per_set
 input1 = Input((numsupportset+1,28,28,1))
--- matchingnetworkwithrelationalembedding.py
+++ matchingnetworkwithrelationalembedding.py
@@ -1,5 +1,9 @@
 """
-Implementation of "Matching network for one short learning" in Keras
+Implementation of "Matching network for one short learning" in Keras.
+A new kind of Full Context Embedding is define here which uses 
+Siamese like pairwise interaction and does a max pooling on these interaction.
+The pooled out is then forwarded to a multi layer perceptron.
+
 __author__ = Chetan Nichkawde
 """
 
@@ -9,8 +13,7 @@ from keras.layers import Dense, Flatten, Input, Lambda
 from keras.layers.merge import Maximum
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-import tensorflow as tf
-from keras.layers.merge import _Merge
+from matchnn import Siamify, MatchEuclidean
 from itertools import combinations
 from collections import defaultdict
 
@@ -18,8 +21,7 @@ bsize = 32 # batch size
 classes_per_set = 5 # classes per set or 5-way
 samples_per_class = 1 # samples per class 1-short
 
-data = dataset.OmniglotNShotDataset(batch_size=bsize,classes_per_set=classes_per_set,samples_per_class=samples_per_class,
-                                    trainsize=64000,valsize=20000)
+data = dataset.OmniglotNShotDataset(batch_size=bsize,classes_per_set=classes_per_set,samples_per_class=samples_per_class,trainsize=64000,valsize=20000)
 
 # Image embedding using Deep Convolutional Network
 conv1 = Conv2D(64,(3,3),padding='same',activation='relu')
@@ -54,7 +56,6 @@ def convembedding(x):
     
     return x
 
-
 # Relational embedding comprising a 4 layer MLP
 d1 = Dense(64,activation='relu')
 dbnrm1 = BatchNormalization()
@@ -76,99 +77,6 @@ def relationalembedding(x):
     x = dbnrm4(x)
     
     return x
-
-class MatchCosine(_Merge):
-    """
-        Matching network with cosine similarity metric
-    """
-    def __init__(self,nway=5,**kwargs):
-        super(MatchCosine,self).__init__(**kwargs)
-        self.eps = 1e-10
-        self.nway = nway
-
-    def build(self, input_shape):
-        if not isinstance(input_shape, list) or len(input_shape) != self.nway+2:
-            raise ValueError('A ModelCosine layer should be called on a list of inputs of length %d'%(self.nway+2))
-
-    def call(self,inputs):
-        """
-        inputs in as array which contains the support set the embeddings, the target embedding as the second last value in the array, and true class of target embedding as the last value in the array
-        """ 
-        similarities = []
-
-        targetembedding = inputs[-2] # embedding of the query image
-        numsupportset = len(inputs)-2
-        for ii in range(numsupportset):
-            supportembedding = inputs[ii] # embedding for i^{th} member in the support set
-
-            sum_support = tf.reduce_sum(tf.square(supportembedding), 1, keep_dims=True)
-            supportmagnitude = tf.rsqrt(tf.clip_by_value(sum_support, self.eps, float("inf"))) #reciprocal of the magnitude of the member of the support 
-
-            sum_query = tf.reduce_sum(tf.square(targetembedding), 1, keep_dims=True)
-            querymagnitude = tf.rsqrt(tf.clip_by_value(sum_query, self.eps, float("inf"))) #reciprocal of the magnitude of the query image
-
-            dot_product = tf.matmul(tf.expand_dims(targetembedding,1),tf.expand_dims(supportembedding,2))
-            dot_product = tf.squeeze(dot_product,[1])
-
-            cosine_similarity = dot_product*supportmagnitude*querymagnitude
-            similarities.append(cosine_similarity)
-
-        similarities = tf.concat(axis=1,values=similarities)
-        softmax_similarities = tf.nn.softmax(similarities)
-        preds = tf.squeeze(tf.matmul(tf.expand_dims(softmax_similarities,1),inputs[-1]))
-        
-        preds.set_shape((inputs[0].shape[0],self.nway))
-
-        return preds
-
-    def compute_output_shape(self,input_shape):
-        input_shapes = input_shape
-        return (input_shapes[0][0],self.nway)
-
-# Bonus: Matching network with Euclidean metrtic
-class MatchEuclidean(_Merge):
-    """
-        Matching network with Euclidean metric
-    """
-    def __init__(self,nway=5,**kwargs):
-        super(MatchEuclidean,self).__init__(**kwargs)
-        self.eps = 1e-10
-        self.nway = nway
-
-    def build(self, input_shape):
-        if not isinstance(input_shape, list) or len(input_shape) != self.nway+2:
-            raise ValueError('A ModelEuclidean layer should be called on a list of inputs of length %d'%(self.nway+2))
-
-    def call(self,inputs):
-        """
-        inputs in as array which contains the support set the embeddings, the target embedding as the second last value in the array, and true class of target embedding as the last value in the array
-        """ 
-        similarities = []
-
-        targetembedding = inputs[-2]
-        numsupportset = len(inputs)-2
-        for ii in range(numsupportset):
-            supportembedding = inputs[ii]
-            dd = tf.negative(tf.sqrt(tf.reduce_sum(tf.square(supportembedding-targetembedding),1,keep_dims=True)))
-
-            similarities.append(dd)
-
-        similarities = tf.concat(axis=1,values=similarities)
-        softmax_similarities = tf.nn.softmax(similarities)
-        preds = tf.squeeze(tf.matmul(tf.expand_dims(softmax_similarities,1),inputs[-1]))
-        
-        preds.set_shape((inputs[0].shape[0],self.nway))
-
-        return preds
-
-    def compute_output_shape(self,input_shape):
-        input_shapes = input_shape
-        return (input_shapes[0][0],self.nway)
-
-# Siamese network like interaction
-class Siamify(_Merge):
-    def _merge_function(self,inputs):
-        return tf.negative(tf.abs(inputs[0]-inputs[1]))
 
 numsupportset = samples_per_class*classes_per_set
 input1 = Input((numsupportset+1,28,28,1))
